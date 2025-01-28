438aa57976accdd54295f5db44debf8878fb2c86
Alexander Safronov <sanya.safron2003@gmail.com>
2025-01-29 00:36:20 +0600

    feature: data and domain layer with blocs

---
+++ lib/app_runner.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:mobile_bootcamp_done/app/app.dart';
+import 'package:mobile_bootcamp_done/common/api_client/api_client.dart';
+import 'package:mobile_bootcamp_done/common/api_client/weather_api_interceptor.dart';
+
+void runWeatherApp() async {
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    final Dio dio = Dio();
+    dio.interceptors.add(WeatherApiInterceptor());
+    final ApiClient apiClient = ApiClient(dio);
+    runApp(WeatherApp(
+      apiClient: apiClient,
+    ));
+  }, (exception, stackTrace) async {
+    log(exception.toString() + stackTrace.toString());
+  });
+}
---
+++ lib/env/env.dart
@@ -0,0 +1,11 @@
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(path: '.env')
+abstract class Env {
+  @EnviedField(varName: 'BASE_URL', obfuscate: true, optional: false)
+  static final String baseurl = _Env.baseurl;
+  @EnviedField(varName: 'API_KEY', obfuscate: true, optional: false)
+  static final String apiKey = _Env.apiKey;
+}
